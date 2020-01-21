5ac971a133ac8b698f381f48aaaf31c37ce05dcd
takeshiD <goriponikeike55@gmail.com>
2020-01-21 22:26:41 +0900

    Slotの理解

--- Vue_基本編/js/v-slot.js
+++ Vue_基本編/js/v-slot.js
@@ -1,7 +1,4 @@
 Vue.component('com0', {
-    data: function() {
-        return {};
-    },
     template: `
     <div>
         <p>hogehoge <slot name="slot1"></slot> hogehoge</p>
@@ -9,6 +6,26 @@ Vue.component('com0', {
     </div>
     `
 })
+
+Vue.component('my-button', {
+    template: `
+    <div>
+        <button><slot>OK</slot></button>
+    </div>
+    `
+})
+
+const book = {
+    template: `
+    <div class="book">
+        <dl>
+            <dt><slot name="title">Display Title</slot></dt>
+            <dd><slot name="summary">Display Summary</slot></dd>
+        </dl>
+    </div>
+    `
+}
+Vue.component('book', book)
 
 new Vue({
     el: '#slot-test'
